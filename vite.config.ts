1190d6d7fd9c167eabab7a064ac0ee6cdff34097
Martin Conradsen <mco@2bm.com>
2025-05-11 09:58:11 +0200

    Updated configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/MartinConradsen.github.io/', // 👈 Important!
+  base: '/',
   plugins: [react()],
 });
