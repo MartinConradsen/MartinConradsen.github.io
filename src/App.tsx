1ad40751e89b092ed87b8ef1113c22e2e66187d0
Martin Conradsen <mco@2bm.com>
2025-05-11 07:57:51 +0200

    React setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Danish Negroni Society</h1>
+      <p>We are a group of enthusiasts dedicated to the perfect balance of gin, vermouth, and Campari.</p>
+    </div>
+  );
+}
+
+export default App;
+
